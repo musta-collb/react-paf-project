cf0f2b1d77a0255f6fa747bbb107e4f18cc02005
Tadashi-hue <remagnavana@gmail.com>
2022-05-17 21:45:50 +0100

    preparing to merge

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import AuthContext from '../contexts/JWTAuthContext'
-
+//git checkout v2
+//git merge testLogin
 const useAuth = () => useContext(AuthContext)
 
 export default useAuth

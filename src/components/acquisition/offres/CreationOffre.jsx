23e20c21897473fd798448e0245ac2138c49f9f0
Tadashi-hue <remagnavana@gmail.com>
2022-05-26 23:37:43 +0100

    so far so good

--- src/components/acquisition/offres/CreationOffre.jsx
+++ src/components/acquisition/offres/CreationOffre.jsx
@@ -4,6 +4,7 @@ import { createOffre } from './apiCalls';
 import  SubmitButton  from '../SubmitButton';
 import SimpleButton from '../SimpleButton';
 const CreationOffre = () => {
+
     //mutation
     const mutation=useMutation(createOffre)
     //Form shits
@@ -51,7 +52,7 @@ const CreationOffre = () => {
                     type="text"  
                     placeholder=" "
                     class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
-                    {...register(`fournisseur.raison_socilale`,{required:"La raison_sociale est obligatoire"})}
+                    {...register(`fournisseur.raison_sociale`,{required:"La raison_sociale est obligatoire"})}
                     />
                     <label for="floating_raison_sociale" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Raison sociale</label>
                 </div>
--- src/components/acquisition/offres/DetailsOffre.jsx
+++ src/components/acquisition/offres/DetailsOffre.jsx
@@ -1,69 +1,181 @@
 import Erreur from "../Erreur";
 import Loading from "../Loading";
 import { useParams } from "react-router-dom";
-import { useQuery } from "react-query";
-import { fetchOffre } from "./apiCalls";
+import { useQuery, useMutation } from "react-query";
+import { fetchOffre, updateOffre } from "./apiCalls";
 import LinkButton from "../LinkButton";
+import { useForm, useFieldArray } from "react-hook-form";
+import { useState, useEffect } from "react";
+import SimpleButton from "../SimpleButton";
+import SubmitButton from "../SubmitButton";
 
 const DetailsOffre = () => {
-  //route related stuff
-  const { idOffre } = useParams();
-  const { id } = useParams();
-  const PARENTURL = `/personnel/${id}/acquisition/offres`;
+    //route related stuff
+    const { idOffre } = useParams();
+    const { id } = useParams();
+    const PARENTURL = `/personnel/${id}/acquisition/offres`;
+    //editionstuff
+    const [isEditing, setIsEditing]=useState(false);
   //data fetching stuff
   const{ data, isLoading, isError, error}=useQuery('detail_offre'+idOffre, ()=>fetchOffre(idOffre));
+  //mutation
+  const mutation=useMutation(updateOffre)
+    //Form shits
+    const{register, handleSubmit, reset, formState, control}=useForm({ mode:'onSubmit'});
+    const{fields, append, remove}=useFieldArray({
+        control,
+        name:'biens'
+    });
+
+    const{errors}=formState;
+
+    const onSubmit=async (form_data)=>{
+        //await mutation.mutate(form_data);
+        setIsEditing(false)
+    }
+
+    useEffect(()=>{
+        if(data){
+            reset(data);
+        }
+    },[data]);
+
 
   if(isLoading) return(<Loading/>);
   if(isError) return(<Erreur/>);
     return (
-        <div className="flex flex-col p-[1em] space-y-2 w-full">
-            <h1 className="font-bold text-2xl text-gray-700 ">Détails de l'offres</h1>
+        <div className="flex flex-col p-[1em] space-y-2 w-full bg-gray-100">
+            <div className="flex justify-between">
+                <h1 className="font-bold text-2xl text-gray-600 ">Détails de l'offres</h1>
+                <SimpleButton settings={{text:"Modifier", color:"bg-yellow-300", action:()=>{setIsEditing(true); reset(data)}}}/>
+            </div>
             {/* en-tête */}
             <div className="flex flex-col px-[1em] ">
-                <div className="flex space-x-2">
+                {isEditing &&<div className="flex"> <SubmitButton settings={{color:"bg-blue-600", text:"Valider" }}/></div>}
+                <div className="flex space-x-2 items-center">
                     <p className="text-sm font-medium">Date: </p>
-                    <div className="text-sm">{data.date}</div>
+                    {isEditing?
+                        <input 
+                        type="date" 
+                        class="text-sm p-0.5 border-gray-200 rounded text-gray-600 m-0.5" 
+                        {...register("date",{required:"La date est obligatoire"})}
+                        />
+                        :
+                        <div className="text-sm">{data.date}</div>
+                    }     
                 </div>
-                <div className="flex space-x-2 mb-2 ">
+                <div className="flex space-x-2 mb-2 items center">
                     <p className="text-sm font-medium">Reference appel d'offre:</p>
+                    {isEditing?
+                    <input 
+                    type="text"  
+                    class="text-sm p-0.5 border-gray-200 rounded text-gray-600 m-0.5" 
+                    {...register("reference_ao",{required:"La référence de l'appel d'offre est obligatoire"})}
+                    />
+                    :
                     <div className="text-sm">{data.reference_ao}</div>
+                    }
                 </div>
             </div>
             {/*Fournisseur */}
             <div className="flex flex-col rounded shadow-lg bg-white pb-2 mb-2">
-                <div className="flex font-bold rounded-t text-xl bg-zinc-200 px-[1em]">Fournisseur</div>
+                <div className="flex font-bold rounded-t text-xl bg-zinc-200 px-[1em] text-gray-600">Fournisseur</div>
                 <div className="flex flex-col px-[1em]">
-                    <div className="flex space-x-2 px-[1em]">
+                    <div className="flex space-x-2 px-[1em] items-center ">
                         <p className="font-medium text-md">Raison social:</p>
+                        {isEditing?
+                        <input 
+                        type="text"  
+                        class="text-sm p-0.5 border-gray-200 rounded text-gray-600 m-0.5" 
+                        {...register(`fournisseur.raison_sociale`,{required:"La raison_sociale est obligatoire"})}
+                        />
+                        :
                         <div className="font-normal text-md">{data.fournisseur.raison_sociale}</div>
+                        }
                     </div>
                     <div className="flex space-x-2 px-[1em]">
                         <p className="font-medium text-md">Adresse:</p>
+                        {isEditing?
+                        <input 
+                        type="text"  
+                        class="text-sm p-0.5 border-gray-200 rounded text-gray-600 m-0.5" 
+                        {...register(`fournisseur.adresse`,{required:"L'adresse est obligatoire"})}
+                        />
+                        :
                         <div className="font-normal text-md">{data.fournisseur.adresse}</div>
+                        }
                     </div>
                     <div className="flex space-x-2 px-[1em]">
                         <p className="font-medium text-md">E-mail:</p>
+                        {isEditing?
+                        <input 
+                        type="text"  
+                        class="text-sm p-0.5 border-gray-200 rounded text-gray-600 m-0.5" 
+                        {...register(`fournisseur.email`,{required:"L'email est obligatoire"})}
+                        />
+                        :
                         <div className="font-normal text-md">{data.fournisseur.email}</div>
+                        }
                     </div>
                     <div className="flex space-x-2 px-[1em]">
                         <p className="font-medium text-md">Téléphone:</p>
+                        {isEditing?
+                        <input 
+                        type="text"  
+                        class="text-sm p-0.5 border-gray-200 rounded text-gray-600 m-0.5" 
+                        {...register(`fournisseur.telephone`,{required:"Le telephone est obligatoire"})}
+                        />
+                        :
                         <div className="font-normal text-md">{data.fournisseur.telephone}</div>
+                        }
                     </div>
                     <div className="flex space-x-2 px-[1em]">
                         <p className="font-medium text-md">Pays:</p>
+                        {isEditing?
+                        <input 
+                        type="text"  
+                        class="text-sm p-0.5 border-gray-200 rounded text-gray-600 m-0.5" 
+                        {...register(`fournisseur.pays`,{required:"Le pays est obligatoire"})}
+                        />
+                        :
                         <div className="font-normal text-md">{data.fournisseur.pays}</div>
+                        }
                     </div>
                     <div className="flex space-x-2 px-[1em]">
                         <p className="font-medium text-md">Secteur:</p>
+                        {isEditing?
+                        <input 
+                        type="text"  
+                        class="text-sm p-0.5 border-gray-200 rounded text-gray-600 m-0.5" 
+                        {...register(`fournisseur.secteur`,{required:"Le secteur est obligatoire"})}
+                        />
+                        :
                         <div className="font-normal text-md">{data.fournisseur.secteur}</div>
+                        }
                     </div>
                     <div className="flex space-x-2 px-[1em]">
                         <p className="font-medium text-md">Nombre année d'expertise:</p>
+                        {isEditing?
+                        <input 
+                        type="text"  
+                        class="text-sm p-0.5 border-gray-200 rounded text-gray-600 m-0.5" 
+                        {...register(`fournisseur.nb_annee_exp`,{required:"Le nombre d'année d'expérience est obligatoire"})}
+                        />
+                        :
                         <div className="font-normal text-md">{data.fournisseur.nb_annee_exp}</div>
+                        }
                     </div>
                     <div className="flex space-x-2 px-[1em]">
                         <p className="font-medium text-md">Code postale:</p>
+                        {isEditing?
+                        <input 
+                        type="text"  
+                        class="text-sm p-0.5 border-gray-200 rounded text-gray-600 m-0.5" 
+                        {...register(`fournisseur.email`,{required:"L'email est obligatoire"})}
+                        />
+                        :
                         <div className="font-normal text-md">{data.fournisseur.code_postale}</div>
+                        }
                     </div>
                 </div>
             </div>
